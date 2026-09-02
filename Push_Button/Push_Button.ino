355777f2824d3e688fb0f1d17e1b7919fa421def
P Yashwanth <yashwanthsrivaishnava@gmail.com>
2026-09-02 18:47:30 +0530

    Add push button example for LED control

---
+++ Push_Button/Push_Button.ino
@@ -0,0 +1,19 @@
+// Push Button Example
+
+const int buttonPin = 2;
+const int ledPin = 13;
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin);
+
+  if (buttonState == LOW) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
