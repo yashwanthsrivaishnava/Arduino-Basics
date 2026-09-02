a5c89122fa0e91b6338076feaf03ce2406396589
P Yashwanth <yashwanthsrivaishnava@gmail.com>
2026-09-02 19:35:35 +0530

    Add DHT11 temperature and humidity monitor code

---
+++ DHT11_Temperature_Humidity/DHT11_Temperature_Humidity.ino
@@ -0,0 +1,36 @@
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+
+  Serial.println("DHT11 Temperature and Humidity Monitor");
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT11 sensor!");
+    delay(2000);
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.println("--------------------");
+
+  delay(2000);
+}
